251715da079ac48e2be507a61ccba5cfc3051b5a
ThomasHeim <60183022+ThomasHeim@users.noreply.github.com>
2020-04-23 11:41:27 -0400

    Pagina funcional
    Pagina completamente funcional, falta añadir contenido

--- assets/js/button-hover.js
+++ assets/js/button-hover.js
@@ -1,5 +1,62 @@
+document.getElementById('ux__first').onmouseover = function()        
+{        
+    document.getElementById('trabajos').style.backgroundImage = "url('/assets/img/ilustracion1.png')";
+    document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
+    document.getElementById('trabajos').style.backgroundPosition = "center";
+    document.getElementById('trabajos').style.backgroundSize = "cover";
+};
 
-function changeFirst(isHover) {
+document.getElementById('ux__first').onmouseout = function()        
+{        
+    document.getElementById('trabajos').style.backgroundImage = "none";
+};
+
+
+document.getElementById('ux__second').onmouseover = function()        
+{        
+    document.getElementById('trabajos').style.backgroundImage = "url('/assets/img/ilustracion2.png')";
+    document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
+    document.getElementById('trabajos').style.backgroundPosition = "center";
+    document.getElementById('trabajos').style.backgroundSize = "cover";
+};
+
+document.getElementById('ux__second').onmouseout = function()        
+{        
+    document.getElementById('trabajos').style.backgroundImage = "none";
+};
+
+
+document.getElementById('ux__third').onmouseover = function()        
+{        
+    document.getElementById('trabajos').style.backgroundImage = "url('/assets/img/ilustracion3.png')";
+    document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
+    document.getElementById('trabajos').style.backgroundPosition = "center";
+    document.getElementById('trabajos').style.backgroundSize = "cover";
+};
+
+document.getElementById('ux__third').onmouseout = function()        
+{        
+    document.getElementById('trabajos').style.backgroundImage = "none";
+};
+
+
+
+document.getElementById('ux__fourth').onmouseover = function()        
+{        
+    document.getElementById('trabajos').style.backgroundImage = "url('/assets/img/ilustracion4.png')";
+    document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
+    document.getElementById('trabajos').style.backgroundPosition = "center";
+    document.getElementById('trabajos').style.backgroundSize = "cover";
+};
+
+document.getElementById('ux__fourth').onmouseout = function()        
+{        
+    document.getElementById('trabajos').style.backgroundImage = "none";
+};
+
+
+
+/*function changeFirst(isHover) {
     if (isHover) {
         document.getElementById('trabajos').style.backgroundImage = "url('/assets/img/ilustracion1.png')";
         document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
@@ -8,4 +65,4 @@ function changeFirst(isHover) {
     } else {
         document.getElementById('trabajos').style.backgroundImage = "none";
     }
-}
+}*/
