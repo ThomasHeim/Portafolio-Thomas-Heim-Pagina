43dfef4afbfcaf2e802f217f82e81466a7b43f7c
ThomasHeim <60183022+ThomasHeim@users.noreply.github.com>
2020-04-24 10:51:39 -0400

    test direccion 2

--- assets/js/button-hover.js
+++ assets/js/button-hover.js
@@ -1,6 +1,6 @@
 document.getElementById('ux__first').onmouseover = function()        
 {        
-    document.getElementById('trabajos').style.backgroundImage = "url('/assets/img/ilustracion1.png')";
+    document.getElementById('trabajos').style.backgroundImage = "url('../assets/img/ilustracion1.png')";
     document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
     document.getElementById('trabajos').style.backgroundPosition = "center";
     document.getElementById('trabajos').style.backgroundSize = "cover";
