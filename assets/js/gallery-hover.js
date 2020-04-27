13de8a4965423db7e5026a6d24e1cffa9d2de2a5
ThomasHeim <60183022+ThomasHeim@users.noreply.github.com>
2020-04-27 11:26:37 -0400

    27042020.1
    JS para el hover de la galeria  listo
    
    Arreglos en el formato

--- assets/js/gallery-hover.js
+++ assets/js/gallery-hover.js
@@ -1,3 +1,4 @@
+//Elemento 1
 document.getElementById('disenoGraficoGaleria1').onmouseover = function()        
 {        
     document.getElementById('descripcionBg1').style.animation = "descripcion__hover 0.5s ease forwards";
@@ -6,8 +7,143 @@ document.getElementById('disenoGraficoGaleria1').onmouseover = function()
 
 document.getElementById('disenoGraficoGaleria1').onmouseout = function()        
 {        
-    document.getElementById('descripcionBg1').style.animation = "descripcion__hover 0.5s ease reverse forwards";
+    document.getElementById('descripcionBg1').style.animation = "none";
     document.getElementById('descripcionBg1').style.width = "0%";
-    document.getElementById('descripcionTexto1').style.animation = "descripcion__hoverTexto 0.5s ease reverse forwards";
+    document.getElementById('descripcionTexto1').style.animation = "none";
+
+};
+
+//Elemento 2
+document.getElementById('disenoGraficoGaleria2').onmouseover = function()        
+{        
+    document.getElementById('descripcionBg2').style.animation = "descripcion__hover 0.5s ease forwards";
+    document.getElementById('descripcionTexto2').style.animation = "descripcion__hoverTexto 0.5s ease forwards";
+};
+
+document.getElementById('disenoGraficoGaleria2').onmouseout = function()        
+{        
+    document.getElementById('descripcionBg2').style.animation = "none";
+    document.getElementById('descripcionBg2').style.width = "0%";
+    document.getElementById('descripcionTexto2').style.animation = "none";
+
+};
+
+//Elemento 3
+document.getElementById('disenoGraficoGaleria3').onmouseover = function()        
+{        
+    document.getElementById('descripcionBg3').style.animation = "descripcion__hover 0.5s ease forwards";
+    document.getElementById('descripcionTexto3').style.animation = "descripcion__hoverTexto 0.5s ease forwards";
+};
+
+document.getElementById('disenoGraficoGaleria3').onmouseout = function()        
+{        
+    document.getElementById('descripcionBg3').style.animation = "none";
+    document.getElementById('descripcionBg3').style.width = "0%";
+    document.getElementById('descripcionTexto3').style.animation = "none";
+
+};
+
+//Elemento 4
+document.getElementById('disenoGraficoGaleria4').onmouseover = function()        
+{        
+    document.getElementById('descripcionBg4').style.animation = "descripcion__hover 0.5s ease forwards";
+    document.getElementById('descripcionTexto4').style.animation = "descripcion__hoverTexto 0.5s ease forwards";
+};
+
+document.getElementById('disenoGraficoGaleria4').onmouseout = function()        
+{        
+    document.getElementById('descripcionBg4').style.animation = "none";
+    document.getElementById('descripcionBg4').style.width = "0%";
+    document.getElementById('descripcionTexto4').style.animation = "none";
+
+};
+
+//Elemento 5
+document.getElementById('disenoGraficoGaleria5').onmouseover = function()        
+{        
+    document.getElementById('descripcionBg5').style.animation = "descripcion__hover 0.5s ease forwards";
+    document.getElementById('descripcionTexto5').style.animation = "descripcion__hoverTexto 0.5s ease forwards";
+};
+
+document.getElementById('disenoGraficoGaleria5').onmouseout = function()        
+{        
+    document.getElementById('descripcionBg5').style.animation = "none";
+    document.getElementById('descripcionBg5').style.width = "0%";
+    document.getElementById('descripcionTexto5').style.animation = "none";
+
+};
+
+//Elemento 6
+document.getElementById('disenoGraficoGaleria6').onmouseover = function()        
+{        
+    document.getElementById('descripcionBg6').style.animation = "descripcion__hover 0.5s ease forwards";
+    document.getElementById('descripcionTexto6').style.animation = "descripcion__hoverTexto 0.5s ease forwards";
+};
+
+document.getElementById('disenoGraficoGaleria6').onmouseout = function()        
+{        
+    document.getElementById('descripcionBg6').style.animation = "none";
+    document.getElementById('descripcionBg6').style.width = "0%";
+    document.getElementById('descripcionTexto6').style.animation = "none";
+
+};
+
+//Elemento 7
+document.getElementById('disenoGraficoGaleria7').onmouseover = function()        
+{        
+    document.getElementById('descripcionBg7').style.animation = "descripcion__hover 0.5s ease forwards";
+    document.getElementById('descripcionTexto7').style.animation = "descripcion__hoverTexto 0.5s ease forwards";
+};
+
+document.getElementById('disenoGraficoGaleria7').onmouseout = function()        
+{        
+    document.getElementById('descripcionBg7').style.animation = "none";
+    document.getElementById('descripcionBg7').style.width = "0%";
+    document.getElementById('descripcionTexto7').style.animation = "none";
+
+};
+
+//Elemento 8
+document.getElementById('disenoGraficoGaleria8').onmouseover = function()        
+{        
+    document.getElementById('descripcionBg8').style.animation = "descripcion__hover 0.5s ease forwards";
+    document.getElementById('descripcionTexto8').style.animation = "descripcion__hoverTexto 0.5s ease forwards";
+};
+
+document.getElementById('disenoGraficoGaleria8').onmouseout = function()        
+{        
+    document.getElementById('descripcionBg8').style.animation = "none";
+    document.getElementById('descripcionBg8').style.width = "0%";
+    document.getElementById('descripcionTexto8').style.animation = "none";
+
+};
+
+//Elemento 9
+document.getElementById('disenoGraficoGaleria9').onmouseover = function()        
+{        
+    document.getElementById('descripcionBg9').style.animation = "descripcion__hover 0.5s ease forwards";
+    document.getElementById('descripcionTexto9').style.animation = "descripcion__hoverTexto 0.5s ease forwards";
+};
+
+document.getElementById('disenoGraficoGaleria9').onmouseout = function()        
+{        
+    document.getElementById('descripcionBg9').style.animation = "none";
+    document.getElementById('descripcionBg9').style.width = "0%";
+    document.getElementById('descripcionTexto9').style.animation = "none";
+
+};
+
+//Elemento 10
+document.getElementById('disenoGraficoGaleria10').onmouseover = function()        
+{        
+    document.getElementById('descripcionBg10').style.animation = "descripcion__hover 0.5s ease forwards";
+    document.getElementById('descripcionTexto10').style.animation = "descripcion__hoverTexto 0.5s ease forwards";
+};
+
+document.getElementById('disenoGraficoGaleria10').onmouseout = function()        
+{        
+    document.getElementById('descripcionBg10').style.animation = "none";
+    document.getElementById('descripcionBg10').style.width = "0%";
+    document.getElementById('descripcionTexto10').style.animation = "none";
 
 };
