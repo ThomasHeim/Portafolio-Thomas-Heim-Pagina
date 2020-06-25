0d4f38c69e913a8224c65a49033dec9aba0e096b
ThomasHeim <60183022+ThomasHeim@users.noreply.github.com>
2020-06-25 11:58:29 -0400

    v.25062020
    se añadio version variable a la seccion trabajos, se añadieron los fondos de trabajo en progreso

--- assets/js/button-hover.js
+++ assets/js/button-hover.js
@@ -1,6 +1,6 @@
 document.getElementById('ux__first').onmouseover = function()        
 {        
-    document.getElementById('trabajos').style.backgroundImage = "url('assets/img/ilustracion1.png')";
+    document.getElementById('trabajos').style.backgroundImage = "url('assets/img/trabajoEnProgreso.png')";
     document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
     document.getElementById('trabajos').style.backgroundPosition = "center";
     document.getElementById('trabajos').style.backgroundSize = "cover";
@@ -14,7 +14,20 @@ document.getElementById('ux__first').onmouseout = function()
 
 document.getElementById('ux__second').onmouseover = function()        
 {        
-    document.getElementById('trabajos').style.backgroundImage = "url('assets/img/ilustracion2.png')";
+    document.getElementById('trabajos').style.backgroundImage = "url('assets/img/trabajoEnProgreso.png')";
+    document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
+    document.getElementById('trabajos').style.backgroundPosition = "center";
+    document.getElementById('trabajos').style.backgroundSize = "cover";
+};
+
+document.getElementById('ux__second-desktop').onmouseout = function()        
+{        
+    document.getElementById('trabajos').style.backgroundImage = "none";
+};
+
+document.getElementById('ux__second-desktop').onmouseover = function()        
+{        
+    document.getElementById('trabajos').style.backgroundImage = "url('assets/img/trabajoEnProgreso.png')";
     document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
     document.getElementById('trabajos').style.backgroundPosition = "center";
     document.getElementById('trabajos').style.backgroundSize = "cover";
