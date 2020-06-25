0420adecdfcf482b5af097305b35faf327554633
ThomasHeim <60183022+ThomasHeim@users.noreply.github.com>
2020-06-25 12:10:37 -0400

    v.2.250620202

--- assets/js/button-hover.js
+++ assets/js/button-hover.js
@@ -39,36 +39,6 @@ document.getElementById('ux__second').onmouseout = function()
 };
 
 
-document.getElementById('ux__third').onmouseover = function()        
-{        
-    document.getElementById('trabajos').style.backgroundImage = "url('assets/img/ilustracion3.png')";
-    document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
-    document.getElementById('trabajos').style.backgroundPosition = "center";
-    document.getElementById('trabajos').style.backgroundSize = "cover";
-};
-
-document.getElementById('ux__third').onmouseout = function()        
-{        
-    document.getElementById('trabajos').style.backgroundImage = "none";
-};
-
-
-
-document.getElementById('ux__fourth').onmouseover = function()        
-{        
-    document.getElementById('trabajos').style.backgroundImage = "url('assets/img/ilustracion4.png')";
-    document.getElementById('trabajos').style.backgroundRepeat = "no-repeat";
-    document.getElementById('trabajos').style.backgroundPosition = "center";
-    document.getElementById('trabajos').style.backgroundSize = "cover";
-};
-
-document.getElementById('ux__fourth').onmouseout = function()        
-{        
-    document.getElementById('trabajos').style.backgroundImage = "none";
-};
-
-
-
 /*function changeFirst(isHover) {
     if (isHover) {
         document.getElementById('trabajos').style.backgroundImage = "url('/assets/img/ilustracion1.png')";
